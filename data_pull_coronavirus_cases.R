273a0f8e00efbc25bd756aef200e8682a859e401
Sam Parm <40672605+parmsam@users.noreply.github.com>
2020-03-31 09:30:24 -0400

    Update data_pull_coronavirus_cases.R

--- data_pull_coronavirus_cases.R
+++ data_pull_coronavirus_cases.R
@@ -7,18 +7,27 @@ library(lubridate)
 
 #declare github csv data links here ####
 #time series data paths
-time_series_19_covid_Confirmed_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-time_series_19_covid_Deaths_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
-time_series_19_covid_Recovered_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+time_series_19_covid_Confirmed_global_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+time_series_19_covid_Deaths_global_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+time_series_19_covid_Recovered_global_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+
+time_series_19_covid_Confirmed_usa_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+time_series_19_covid_Deaths_usa_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+time_series_19_covid_Recovered_usa_path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_US.csv"
+
 #latest daily report data paths
 yesterday <- format(today()-1, "%m-%d-%Y")
 csse_covid_19_daily_reports_path <- str_c("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/",
       yesterday, ".csv",sep="")
 
 #read csv data based on paths declared above ####
-time_series_19_covid_Confirmed <- read_csv(time_series_19_covid_Confirmed_path)
-time_series_19_covid_Deaths <- read_csv(time_series_19_covid_Deaths_path)
-time_series_19_covid_Recovered <- read_csv(time_series_19_covid_Recovered_path)
+time_series_19_covid_Confirmed_global <- read_csv(time_series_19_covid_Confirmed_global_path)
+time_series_19_covid_Deaths_global <- read_csv(time_series_19_covid_Deaths_global_path)
+time_series_19_covid_Recovered_global <- read_csv(time_series_19_covid_Recovered_global_path)
+
+time_series_19_covid_Confirmed_usa <- read_csv(time_series_19_covid_Confirmed_usa_path)
+time_series_19_covid_Deaths_usa <- read_csv(time_series_19_covid_Deaths_usa_path)
+time_series_19_covid_Recovered_usa <- read_csv(time_series_19_covid_Recovered_usa_path)
 
 csse_covid_19_daily_reports <- read_csv(csse_covid_19_daily_reports_path)
 
